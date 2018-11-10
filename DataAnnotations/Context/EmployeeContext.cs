1ff99a920822d7f24ea4a72c43647851037a272e
unknown <ducnnd00468@fpt.edu.vn>
2018-11-09 18:33:34 -0800

    firstcomment

---
+++ DataAnnotations/Context/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using DataAnnotations.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DataAnnotations.Context
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
